295168b79f2158f801b5225c1569ce6b47a1e491
wesllanSilva <wesllan90@gmail.com>
2021-01-06 20:24:04 -0300

    adicionado  while e switch para as escolhas

--- Program.cs
+++ Program.cs
@@ -10,8 +10,34 @@ namespace revisaoCsharp
             Console.WriteLine("1- Inserir novo aluno");
             Console.WriteLine("2- Listar alunos");
             Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X Sair");
 
             string opcaoUsuario = Console.ReadLine();
+
+            while(opcaoUsuario.ToUpper() != "X"){
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicinar aluno
+                        break;
+                    case "2":
+                        //TODO: lista alunos
+                        break;
+                    case "3":
+                        //TODO: calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                Console.WriteLine("Informe a opção desejada:");
+                Console.WriteLine("1- Inserir novo aluno");
+                Console.WriteLine("2- Listar alunos");
+                Console.WriteLine("3- Calcular média geral");
+                Console.WriteLine("X Sair");
+
+                opcaoUsuario = Console.ReadLine();
+            }
         }
     }
 }
