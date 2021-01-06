351d1e4686eef751b1be564aa763d0d936e486b5
wesllanSilva <wesllan90@gmail.com>
2021-01-06 18:11:07 -0300

    Inicio da aplicação colocando opções no menu

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace revisaoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+
+            string opcaoUsuario = Console.ReadLine();
+        }
+    }
+}
