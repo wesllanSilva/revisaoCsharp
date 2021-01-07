875bac95795a879d3866c50e384d28684346e9ea
wesllanSilva <wesllan90@gmail.com>
2021-01-06 22:00:46 -0300

    nova branch

--- Program.cs
+++ Program.cs
@@ -6,16 +6,11 @@ namespace revisaoCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a opção desejada:");
-            Console.WriteLine("1- Inserir novo aluno");
-            Console.WriteLine("2- Listar alunos");
-            Console.WriteLine("3- Calcular média geral");
-            Console.WriteLine("X Sair");
+            string opcaoUsuario = obterOpcaoUsuario();
 
-            string opcaoUsuario = Console.ReadLine();
-
-            while(opcaoUsuario.ToUpper() != "X"){
-                switch(opcaoUsuario)
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
                 {
                     case "1":
                         //TODO: adicinar aluno
@@ -29,15 +24,22 @@ namespace revisaoCsharp
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
-                
-                Console.WriteLine("Informe a opção desejada:");
-                Console.WriteLine("1- Inserir novo aluno");
-                Console.WriteLine("2- Listar alunos");
-                Console.WriteLine("3- Calcular média geral");
-                Console.WriteLine("X Sair");
 
-                opcaoUsuario = Console.ReadLine();
+                opcaoUsuario = obterOpcaoUsuario();
             }
+        }
+
+        private static string obterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X Sair");
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
