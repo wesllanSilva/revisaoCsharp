01f1f8dd5657c6e8f714775c009217a7522b3f06
wesllanSilva <wesllan90@gmail.com>
2021-01-06 23:41:38 -0300

    colocado elementos para case 1 e case 3

---
+++ Aluno.cs
@@ -0,0 +1,9 @@
+namespace revisaoCsharp
+{
+    internal struct Aluno
+    {
+        public string Nome { get; set; }
+
+        public decimal Nota { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace revisaoCsharp
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[6];
+            int indiceAluno = 0;
             string opcaoUsuario = obterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
@@ -13,10 +15,34 @@ namespace revisaoCsharp
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: adicinar aluno
+                        Console.WriteLine("Informe o nome do aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser Decimal.");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
                         break;
                     case "2":
-                        //TODO: lista alunos
+                        foreach(var a in alunos)
+                        {
+                            if(!a.Nome.Equals(""))
+                            {
+                                Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                            
+                        }
                         break;
                     case "3":
                         //TODO: calcular média geral
@@ -25,12 +51,14 @@ namespace revisaoCsharp
                         throw new ArgumentOutOfRangeException();
                 }
 
+
                 opcaoUsuario = obterOpcaoUsuario();
             }
         }
 
         private static string obterOpcaoUsuario()
         {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1- Inserir novo aluno");
             Console.WriteLine("2- Listar alunos");
